a7174b0177b0e2d29886245b3dc215d1cfd920b5
wasim0028 <wasimakram0028>
2024-01-04 05:59:46 +0000

    First commit for day1

---
+++ 3.js
@@ -0,0 +1,6 @@
+This tutorial covers every version of JavaScript:
+
+The Original JavaScript ES1 ES2 ES3 (1997-1999)
+The First Main Revision ES5 (2009)
+The Second Revision ES6 (2015)
+The Yearly Additions (2016, 2017 ... 2021, 2022)
